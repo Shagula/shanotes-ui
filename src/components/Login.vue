0b0721549eec04eea9dcb754d14be1883861067f
HttoHu <huyuantao@outlook.com>
2022-06-11 08:57:33 +0800

    update: improve login-ui

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,13 @@
 <template>
-    <div id="main">
-        <h1>Shanotes</h1> <br />
-        <span class="label">用户名</span> <br />
-        <el-input size="large" v-model="username" /> <br />
-        <span class="label">密码</span> <br />
-        <el-input type="password" size="large" v-model="password" /> <br />
-        <el-button size="large" type="primary" @click="handleSignup">登录</el-button>
+    <div id="page">
+        <div id="main">
+            <h1>Shanotes</h1> <br />
+            <span class="label">用户名</span> <br />
+            <el-input size="large" v-model="username" /> <br />
+            <span class="label">密码</span> <br />
+            <el-input type="password" size="large" v-model="password" /> <br />
+            <el-button size="large" type="primary" @click="handleSignup">登录</el-button>
+        </div>
     </div>
 </template>
 <script>
@@ -45,21 +47,30 @@ export default {
 </script>
 
 <style scoped>
+#page {
+    background-image: url(@/assets/login-background.png);
+    background-position: center;
+    height: 100vh;
+    width: 100vw;
+    overflow: hidden;
+}
+#main {
+    margin: 0 auto;
+    width: 40%;
+    /* background: #f7f8f9; */
+    background: rgba(119,136,153,0.5);
+    margin-top: 100px;
+}
 h1 {
-    padding-top: 20px;
     margin-bottom: 20px;
+    padding-top: 20px;
 }
 .label {
     font-size: 20px;
     width: 100px;
     margin-bottom: 20px;
 }
-#main {
-    margin: 0 auto;
-    width: 40%;
-    /* background: #f7f8f9; */
-    background: pink;
-}
+
 .el-input {
     width: 300px;
     margin-top: 20px;
